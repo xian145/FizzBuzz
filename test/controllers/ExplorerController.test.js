f267d143295d3e3a1625849855eca06de59e24eb
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-27 13:53:59 -0700

    se agrego una prueba para el controlador ExplorerController

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,21 @@
+const ExplorerController = require("../../lib/controllers/ExplorerController");
+const ExplorerService = require("../../lib/services/ExplorerService");
+
+describe("Test para explorerController", () => {
+    test("1. explorers by mission", () => {
+        const explorer = ExplorerController.getExplorersByMission('node')
+        expect(explorer).toHaveLength(10);
+    });
+
+    test("2. Amount of explorers by mission", () => {
+        const explorers = ExplorerController.getExplorersAmonutByMission('node')
+        expect(explorers).toBe(10);
+    });
+
+    test("3. Mentions ajolonauta", () => {
+        const explorers = ExplorerController.getExplorersUsernamesByMission('node')
+        const ajolonautas = ["ajolonauta1"];
+        expect(explorers).toEqual(expect.arrayContaining(ajolonautas));
+    });
+
+});
