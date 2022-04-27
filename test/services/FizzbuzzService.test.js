22e3b5faa2ebdca1dce14710ca069a12830c00fc
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-27 15:50:50 -0700

    se agregaron pruebas para la comprobacion del nuevo metodo de fizzbuzz en el cual se hace una consulta ahora por numero

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -9,4 +9,28 @@ describe("Test para explorer service", () => {
         expect(listOfExplorersTrick[1].trick).not.toBe(undefined);
     });
 
+    test("2. fizzbuzz ", () => {
+        const consult = 15
+        const trick = FizzbuzzService.applyValidationInNumber(consult)
+        expect(trick.trick).toBe("FIZZBUZZ");
+    });
+
+    test("3. buzz", () => {
+        const consult = 10
+        const trick = FizzbuzzService.applyValidationInNumber(consult)
+        expect(trick.trick).toBe("BUZZ");
+    });
+
+    test("4. fizz", () => {
+        const consult = 9
+        const trick = FizzbuzzService.applyValidationInNumber(consult)
+        expect(trick.trick).toBe("FIZZ");
+    });
+
+    test("5. 2", () => {
+        const consult = 2
+        const trick = FizzbuzzService.applyValidationInNumber(consult)
+        expect(trick.trick).toBe(2);
+    });
+
 });
