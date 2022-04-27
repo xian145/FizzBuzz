731a7e77b491f3b306d59bc268a75403bd192e69
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-26 20:07:37 -0700

    se arreglo el archivop app.js siplificando mediante refactorizacion

--- app.js
+++ app.js
@@ -1,58 +1,15 @@
 const Reader = require("./lib/utils/Reader");
 const ExplorerService = require("./lib/services/ExplorerService");
+const FizzbuzzService = require("./lib/services/FizzbuzzService")
 
 //Clase con la que obtenemos la lista de explorers
 const explorers = Reader.readJsonFile("explorers.json");
 
-//filtrado sobre la lista de explorers
-ExplorerService.filterByMission(explorers, "node");
-ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+//Filtrado sobre la lista de explorers
+const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+const numberOfExplorerinNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+const explorerUserNameInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
 
-// Part 5: Get a new list of explorers in node, if the score numbers is divisible by 3, I need a new propery called trick, and the value assigned is FIZZ, if not the value should be the score itself.
-// Score: 3, Trick: FIZZ.
-// Score: 4, Trick: 4.
-// Score: 5, Trick: 5.
-
-const assignFizzTrick = function(explorer){
-    if(explorer.score % 3 === 0){ //ve si es divisible entre tres
-        explorer.trick = "FIZZ"; //agrega la propiedad trick
-        return explorer;
-    }else{
-        explorer.trick = explorer.score;
-        return explorer;
-    }
-};
-
-const explorersInNodeAndFizzTrick = explorersInNode.map((explorer) => assignFizzTrick(explorer)); //a la lista de los explorers que estan en node le hace un map modificando la propiedad de trick, solo si cumple lo establecido en la funcion
-//console.log(explorersInNodeAndFizzTrick);
-
-// Part 6: Get a new list of explorers in node if the score number is divisible by 5, we need to set a new property called trick and set the value BUZZ, if not this value should be just the score
-//
-const assignBuzzTrick = function(explorer){
-    if(explorer.score%5 === 0){
-        explorer.trick = "BUZZ";
-        return explorer;
-    }else{
-        explorer.trick = explorer.score;
-        return explorer;
-    }
-};
-
-const explorersInNodeAndBuzzTrick = explorersInNode.map((explorer) => assignBuzzTrick(explorer));
-
-//Part7: Get a new list of explorers in Node, if the score number is divisible by 3 AND by 5 we need to set a new property called FIZZBUZZ, if not this value should be the same score value
-
-const assignFizzBuzzTrick = function(explorer){
-    if(explorer.score%5 === 0 && explorer.score%3 === 0){
-        explorer.trick = "FIZZBUZZ";
-        return explorer;
-    }else{
-        explorer.trick = explorer.score;
-        return explorer;
-    }
-};
-
-const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) => assignFizzBuzzTrick(explorer));
-
-// Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO
+//Asignacion de propiedad trick
+const listOfExplorersTrick = explorersInNode.map((explorer) => FizzbuzzService.applyValidationInExplorer(explorer))
+console.log(listOfExplorersTrick);
