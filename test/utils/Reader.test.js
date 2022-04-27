31d0028a23f79c5d775c9c789fed84b7ab088e82
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-26 17:55:59 -0700

    se agrego una prueba para comprobar que si lea el archivo

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require('../../lib/utils/Reader')
+
+describe('Test para Reader', () => {
+    test('1. has to have something', () => {
+        const explorers = Reader.readJsonFile('explorers.json')
+        expect(explorers).not.toBe(undefined)
+    })
+})
