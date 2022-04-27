e719c7c5707e2376618d09b06a3f6e28bbc9f487
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-26 20:05:59 -0700

    se creo una prueba para fizzbuzzService

---
+++ test/services/FizzbuzzService.test.js
@@ -0,0 +1,12 @@
+const Reader = require("../../lib/utils/Reader");
+const FizzbuzzService = require("../../lib/services/FizzbuzzService");
+
+describe("Test para explorer service", () => {
+
+    test("1. ", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const listOfExplorersTrick = explorers.map((explorer) => FizzbuzzService.applyValidationInExplorer(explorer))
+        expect(listOfExplorersTrick[1].trick).not.toBe(undefined);
+    });
+
+});
